4531fc9d7cfee6ed22fb8855ef683689f6388c7d
Ibrahim <ibs@pop-os.localdomain>
2023-06-07 16:08:01 +0100

    Updated cases for user inputs. Added new machine model

--- manual_rps.py
+++ manual_rps.py
@@ -1,25 +1,25 @@
 import random
 
 def get_computer_choice():
-    return random.choice(["rock", "paper", "scissors"])
+    return random.choice(["Rock", "Paper", "Scissors"])
 
 def get_user_choice():
     choice = input("Choose rock, paper or scissors")
-    if choice not in ["rock", "paper", "scissors"]:
+    if choice not in ["Rock", "Paper", "Scissors"]:
         print("Incorrect choice, try again!")
         get_user_choice()
     return choice
 
-#get_user_choice()
 
 def get_winner(computer_choice, user_choice):
     if computer_choice == user_choice:
         print("It's a tie!")
         return
-    elif (computer_choice == "rock" and user_choice == "paper") or (computer_choice == "paper" and user_choice == "scissors") or (computer_choice == "scissors" and user_choice == "rock"):
+    elif (computer_choice == "Rock" and user_choice == "Paper") or (computer_choice == "Paper" and user_choice == "Scissors") or (computer_choice == "Scissors" and user_choice == "Rock"):
         print("You won!")
         return
     else:
         print("You lost!")
 
-get_winner(get_computer_choice(), get_user_choice())
+def play():
+    get_winner(get_computer_choice(), get_user_choice())
